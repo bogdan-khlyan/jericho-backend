c2e99e54b4d2194dc7ed9ed6391732d91078bc4e
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2025-10-03 23:42:02 +0300

    sa

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,25 +1,15 @@
-// /opt/org-structure/strapi/ecosystem.config.cjs
 module.exports = {
     apps: [
         {
-            name: 'org-structure-strapi',
-            cwd: '/opt/org-structure/strapi',          // ОБЯЗАТЕЛЬНО!
+            name: 'jericho-backend',
             script: 'npm',
-            args: 'start',
+            args: 'run start',
             env: {
                 NODE_ENV: 'production',
-                // чтобы не зависеть от .env, продублируем самое критичное
-                HOST: '127.0.0.1',                       // безопаснее чем 0.0.0.0
-                PORT: '5561',
-                DATABASE_CLIENT: 'sqlite',
-                DATABASE_FILENAME: '/opt/org-structure/strapi/storage/prod.db',
-                NODE_OPTIONS: '--max-old-space-size=4096'
-            },
-            // опционально, но полезно:
-            autorestart: true,
-            watch: false,
-            max_memory_restart: '600M',
-            time: true,
+                ENV_PATH: '.env.devserver',
+                PORT: 1338,
+                HOST: '0.0.0.0'
+            }
         }
     ]
-};
+}
